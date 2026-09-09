e5dac0ed61ca06e3b7e15179077ef993b5b28d07
7klinnnn <1549194045@qq.com>
2026-09-09 10:02:14 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const panel = document.createElement("div");
-panel.id = "phoneos-panel";
-
-panel.innerHTML = `
-<iframe src="/scripts/extensions/third-party/vampire/index.html"></iframe>
-`;
-
-document.body.appendChild(panel);
